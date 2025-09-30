ccf332bf3b729238bc00dcb759e52166fa17c357
antohub-r72 <snehithan@gmail.com>
2025-09-30 06:51:03 -0400

    updates

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,13 +7,25 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    anger = emotions['anger']
-    disgust = emotions['disgust']
-    fear = emotions['fear']
-    joy = emotions['joy']
-    sadness = emotions['sadness']
-    dominant_emotion = max(emotions, key = emotions.get)
-    return { 'anger': anger,'disgust': disgust,'fear': fear,'joy': joy,'sadness': sadness,
-     'dominant_emotion': dominant_emotion}
-    return response.text
+    if response.status_code == 200:
+
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        dominant_emotion = max(emotions, key = emotions.get)
+
+        return { 'anger': anger,'disgust': disgust,'fear': fear,'joy': joy,'sadness': sadness,
+            'dominant_emotion': dominant_emotion}
+    elif response.status_code == 400:
+        return {
+                'anger': None,
+                'disgust': None, 
+                'fear': None, 
+                'joy': None, 
+                'sadness': None, 
+                'dominant_emotion': None
+                }
+
